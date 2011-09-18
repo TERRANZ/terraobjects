d7e21f2d50c002f4e9d158e1389eb84ef0e77a9c
Вадим <vterranz@gmail.com>
2011-09-18 16:58:39 +0400

    added j2se proh

---
+++ j2se/terraobjects/src/ru/terraobjects/server/Main.java
@@ -0,0 +1,16 @@
+package ru.terraobjects.server;
+
+/**
+ *
+ * @author terranz
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
