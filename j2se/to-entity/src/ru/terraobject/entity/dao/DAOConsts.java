ebdd763a0e0b31f4e4ca01a9249c0ba51c3c92c5
root <root@homedebian.(none)>
2011-10-07 13:49:20 +0400

    hash scan client now can do md5 hash scan, added remove* commands to managers, added delete* commands to DAO

--- j2se/to-entity/src/ru/terraobject/entity/dao/DAOConsts.java
+++ j2se/to-entity/src/ru/terraobject/entity/dao/DAOConsts.java
@@ -4,27 +4,40 @@ package ru.terraobject.entity.dao;
  *
  * @author terranz
  */
-public interface DAOConsts {
-    public final String SELECT_ALL_OBJECTS = "select * from object";
-    public final String SELECT_OBJECT_BY_ID = "select * from object where object_id = ?";
-    public final String SELECT_OBJECTS_BY_TEMPLATE_ID = "select * from object where object_template_id = ?";
-    public final String SELECT_OBJECTS_COUNT_BY_TEMPLATE_ID = "select COUNT(object_template_id) from object where object_template_id = ?";
-    public final String SELECT_ALL_PROP_TYPES = "select * from prop_type";
-    public final String SELECT_PROP_TYPE_BY_ID = "select * from prop_type where prop_type_id = ?";
-    public final String SELECT_PROP_TYPE_BY_NAME = "select * from prop_type where prop_type_name like '%?%'";
-    public final String SELECT_ALL_PROPERTIES = "select * from property";
-    public final String SELECT_PROPERTY_BY_ID = "select * from property where prop_id = ?";
-    public final String SELECT_ALL_OBJECT_TEMPLATE_PROPS = "select * from object_template_props";
-    public final String SELECT_OBJECT_TEMPLATE_PROPS_BY_ID = "select * from object_template_props where "
-	    + "object_template_props_id = ?";
-    public final String SELECT_OBJECT_TEMPLATE_PROPS_BY_TEMPLATE_ID = "select * from object_template_props where "
-	    + "object_template_id = ?";
-    public final String SELECT_ALL_OBJECT_TEMPLATE = "select * from object_template";
-    public final String SELECT_OBJECT_TEMPLATE_BY_ID = "select * from object_template where "
-	    + "object_template_id = ?";
-    public final String SELECT_OBJECT_PROPS_BY_OBJECT_ID = "select * from object_props where object_id = ?";
-    public final String SELECT_OBJECT_PROP_BY_OBJECT_ID_AND_PROP_ID =
-	    "select * from object_props where object_id = ? AND prop_id = ?";
-    public final String SELECT_ALL_OBJECT_PROPS = "select * from object_props";
-    public final String SELECT_OBJECT_PROPS_BY_PROP_ID = "select * from object_props where prop_id = ?";
+public interface DAOConsts
+{
+
+    public final static String SELECT_ALL_OBJECTS = "select * from object";
+    public final static String SELECT_OBJECT_BY_ID = "select * from object where object_id = ?";
+    public final static String SELECT_OBJECTS_BY_TEMPLATE_ID = "select * from object where object_template_id = ?";
+    public final static String SELECT_OBJECTS_COUNT_BY_TEMPLATE_ID = "select COUNT(object_template_id) from object where object_template_id = ?";
+    public final static String SELECT_ALL_PROP_TYPES = "select * from prop_type";
+    public final static String SELECT_PROP_TYPE_BY_ID = "select * from prop_type where prop_type_id = ?";
+    public final static String SELECT_PROP_TYPE_BY_NAME = "select * from prop_type where prop_type_name like '%?%'";
+    public final static String SELECT_ALL_PROPERTIES = "select * from property";
+    public final static String SELECT_PROPERTY_BY_ID = "select * from property where prop_id = ?";
+    public final static String SELECT_ALL_OBJECT_TEMPLATE_PROPS = "select * from object_template_props";
+    public final static String SELECT_OBJECT_TEMPLATE_PROPS_BY_ID = "select * from object_template_props where "
+            + "object_template_props_id = ?";
+    public final static String SELECT_OBJECT_TEMPLATE_PROPS_BY_TEMPLATE_ID = "select * from object_template_props where "
+            + "object_template_id = ?";
+    public final static String SELECT_ALL_OBJECT_TEMPLATE = "select * from object_template";
+    public final static String SELECT_OBJECT_TEMPLATE_BY_ID = "select * from object_template where "
+            + "object_template_id = ?";
+    public final static String SELECT_OBJECT_PROPS_BY_OBJECT_ID = "select * from object_props where object_id = ?";
+    public final static String SELECT_OBJECT_PROP_BY_OBJECT_ID_AND_PROP_ID =
+            "select * from object_props where object_id = ? AND prop_id = ?";
+    public final static String SELECT_ALL_OBJECT_PROPS = "select * from object_props";
+    public final static String SELECT_OBJECT_PROPS_BY_PROP_ID = "select * from object_props where prop_id = ?";
+    //TODO backup needed before removing
+    //set `deleted` flag to their records, or move to object_backup?    
+    public final static String REMOVE_ALL_OBJECTS = "truncate object";
+    public final static String REMOVE_ALL_OBJECT_PROPS = "truncate object_props";
+    public final static String REMOVE_OBJECT_BY_TEMPLATE_ID = "delete from object where object_template_id = ?";
+    public final static String REMOVE_OBJECT_BY_ID = "delete from object where object_id = ?";
+    public final static String REMOVE_OBJECT_PROP_BY_OBJECT_ID = "delete from object_props where object_id = ?";
+    //TODO backup needed here, coz it's delete property from all objects
+    public final static String REMOVE_OBJECT_PROP_BY_PROP_ID = "delete from object_props where prop_id = ?";
+    public final static String REMOVE_OBJECT_PROP_BY_OBJECT_ID_AND_PROP_ID =
+            "delete from object_props where object_id = ? AND prop_id = ?";
 }
--- j2se/to-solutions/src/ru/terraobjects/solutions/hashscan/HSServerThread.java
+++ j2se/to-solutions/src/ru/terraobjects/solutions/hashscan/HSServerThread.java
@@ -7,14 +7,10 @@ import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.Connection;
 import java.util.List;
 import ru.terraobject.entity.TOObject;
-import ru.terraobject.entity.TOObjectProperty;
-import ru.terraobject.entity.TOProperty;
-import ru.terraobject.entity.TOPropertyType;
 import ru.terraobject.entity.dao.TOObjectsManager;
 import ru.terraobject.entity.dao.TOPropertiesManager;
 
@@ -28,87 +24,109 @@ public class HSServerThread implements Runnable
     private final int OPCODE_END = 0;
     private final int OPCODE_HASH = 1;
     private final int OPCODE_GET_OBJ = 2;
+    private final int OPCODE_CLEAN_FULL = 100;
+    private final int OPCODE_REMOVE_OBJ = 101;
+    private final int OPCODE_REMOVE_OBJ_BY_TEMPLATE = 102;
     private Socket serverSocket;
     private Connection conn;
 
     public HSServerThread(Socket s, Connection conn)
     {
-	this.serverSocket = s;
-	this.conn = conn;
+        this.serverSocket = s;
+        this.conn = conn;
     }
 
     public void run()
     {
-	try
-	{
-	    InputStream sin = serverSocket.getInputStream();
-	    OutputStream sout = serverSocket.getOutputStream();
-	    DataInputStream in = new DataInputStream(new BufferedInputStream(sin));
-	    DataOutputStream out = new DataOutputStream(new BufferedOutputStream(sout));
-	    TOObjectsManager.getInstance().setConnection(conn);
-	    TOPropertiesManager.getInstance().setConnection(conn);
-	    boolean exit = false;
-	    while (!exit)
-	    {
-		int opcode = 0;
-		opcode = in.readInt();
-		switch (opcode)
-		{
-		    case OPCODE_HASH:
-		    {
-			int size = in.readInt();
-			char[] filename = new char[size];
-			for (int i = 1; i <= size; i++)
-			{
-			    filename[i - 1] = in.readChar();
-			}
-			size = in.readInt();
-			char[] hash = new char[size];
-			for (int i = 1; i <= size; i++)
-			{
-			    hash[i - 1] = in.readChar();
-			}
-			Integer newobjId = TOObjectsManager.getInstance().createNewObject(2).getObjectId();
-			TOObjectsManager.getInstance().setPropertyValue(newobjId, 1, String.valueOf(filename));
-			TOObjectsManager.getInstance().setPropertyValue(newobjId, 3, String.valueOf(hash));
-		    }
-		    break;
-		    case OPCODE_END:
-			exit = true;
-			break;
-		    case OPCODE_GET_OBJ:
-		    {
-			Integer objCount = TOObjectsManager.getInstance().getObjectsCountByTemplateId(2);
-			out.writeInt(objCount);
-			System.out.println("Sent count: "+String.valueOf(objCount));
-			if (objCount != 0)
-			{
-			    List<TOObject> objects = TOObjectsManager.getInstance().getAllObjectsByTemplateId(2);
-			    for (TOObject obj : objects)
-			    {
-				String name = TOPropertiesManager.getInstance().getObjectProperty(obj.getObjectId(), 1).getStringVal();
-				String hash = TOPropertiesManager.getInstance().getObjectProperty(obj.getObjectId(), 3).getStringVal();
-				out.writeUTF(name);
-				out.writeUTF(hash);
-				out.flush();
-			    }
-			}
-		    }
-		    break;
-		}
-	    }
-	    out.close();
-	    in.close();
-	    serverSocket.close();
+        try
+        {
+            InputStream sin = serverSocket.getInputStream();
+            OutputStream sout = serverSocket.getOutputStream();
+            DataInputStream in = new DataInputStream(new BufferedInputStream(sin));
+            DataOutputStream out = new DataOutputStream(new BufferedOutputStream(sout));
+            TOObjectsManager.getInstance().setConnection(conn);
+            TOPropertiesManager.getInstance().setConnection(conn);
+            boolean exit = false;
+            while (!exit)
+            {
+                int opcode = 0;
+                try
+                {
+                    opcode = in.readInt();
+                } catch (Exception e)
+                {
+                    exit = true;
+                }
+                switch (opcode)
+                {
+                    case OPCODE_HASH:
+                    {
+                        String filename = in.readUTF();
+                        String hash = in.readUTF();
+                        Integer newobjId = TOObjectsManager.getInstance().createNewObject(2).getObjectId();
+                        TOObjectsManager.getInstance().setPropertyValue(newobjId, 1, filename);
+                        TOObjectsManager.getInstance().setPropertyValue(newobjId, 3, hash);
+                        System.out.println("Created new object: " + filename + " hash: " + hash);
+                    }
+                    break;
+                    case OPCODE_END:
+                        exit = true;
+                        break;
+                    case OPCODE_GET_OBJ:
+                    {
+                        Integer objCount = TOObjectsManager.getInstance().getObjectsCountByTemplateId(2);
+                        out.writeInt(objCount);
+                        System.out.println("Sent count: " + String.valueOf(objCount));
+                        if (objCount != 0)
+                        {
+                            List<TOObject> objects = TOObjectsManager.getInstance().getAllObjectsByTemplateId(2);
+                            for (TOObject obj : objects)
+                            {
+                                String name = TOPropertiesManager.getInstance().getObjectProperty(obj.getObjectId(), 1).getStringVal();
+                                String hash = TOPropertiesManager.getInstance().getObjectProperty(obj.getObjectId(), 3).getStringVal();
+                                out.writeUTF(name);
+                                out.writeUTF(hash);
+                                out.flush();
+                            }
+                        }
+                    }
+                    break;
+                    case OPCODE_CLEAN_FULL:
+                    {
+                        TOObjectsManager.getInstance().removeAllObjects();
+                    }
+                    break;
+                    case OPCODE_REMOVE_OBJ:
+                    {
+                        int oId = in.readInt();
+                        TOObjectsManager.getInstance().removeObjectWithProps(oId);
+                    }
+                    break;
+                    case OPCODE_REMOVE_OBJ_BY_TEMPLATE:
+                    {
+                        int templateId = in.readInt();
+                        TOObjectsManager.getInstance().removeObjectsByTemplate(templateId);
+                    }
+                    break;
+                    default:
+                    {
+                        System.out.println("Unknow opcode: " + String.valueOf(opcode));
+                    }
+                    break;
+                }
+            }
+            out.close();
+            in.close();
+            serverSocket.close();
 
-	} catch (EOFException e)
-	{
-	    //System.out.println("init error: " + e.getStackTrace().toString());
-	    e.printStackTrace();
-	} // вывод исключений
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	}
+        } catch (EOFException e)
+        {
+            //System.out.println("init error: " + e.getStackTrace().toString());
+            e.printStackTrace();
+        } // вывод исключений
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
