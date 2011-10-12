3dc893fa22e25be033530010ba35511c53698352
Вадим <vterranz@gmail.com>
2011-10-13 01:23:43 +0400

    working on helper class started

---
+++ j2se/to-entity/src/ru/terraobjects/entity/dao/TOObjectsHelper.java
@@ -0,0 +1,21 @@
+package ru.terraobjects.entity.dao;
+
+/**
+ *
+ * @author terranz
+ */
+public class TOObjectsHelper
+{
+
+    public Object loadObject(Class objectClass, Integer id)
+    {
+	//loading object from db with props and construct new Object
+	return null;
+    }
+
+    public Integer storeObject(Class objectClass)
+    {
+	//deconstruct object to props and store it into db
+	return null;
+    }
+}
