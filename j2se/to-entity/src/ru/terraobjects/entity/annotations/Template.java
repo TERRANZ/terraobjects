e0dcbd5bb303bfcf6d41345bd9902e3ee77167ea
Вадим <vterranz@gmail.com>
2011-10-13 08:01:12 +0400

    annotation and example of using added

---
+++ j2se/to-entity/src/ru/terraobjects/entity/annotations/Template.java
@@ -0,0 +1,20 @@
+package ru.terraobjects.entity.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author terranz
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Template {
+    String name() default "";
+    String id();
+}
