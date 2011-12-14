fac0bfb272caf5f056814d16cec60e31919afcb9
Вадим <vterranz@gmail.com>
2011-12-14 15:14:46 +0400

    removed all from master but to-entity part2

--- qt-terrastore-solution-client/object.cpp
+++
@@ -1,7 +0,0 @@
-#include "object.h"
-
-Object::Object(quint32 id,quint32 parentId)
-{
-    this->id = id;
-    this->parentId = parentId;
-}
--- qt-terrastore-solution-client/property.cpp
+++
@@ -1,7 +0,0 @@
-#include "property.h"
-
-Property::Property(quint32 id,quint32 typeId)
-{
-    this->id = id;
-    this->typeId = typeId;
-}
