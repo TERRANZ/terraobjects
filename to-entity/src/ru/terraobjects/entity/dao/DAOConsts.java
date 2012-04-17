aa7de527a4bfb0303c67903456642609a3757d31
VKorostelev <vkorostelev@webzavod.ru>
2012-04-17 18:26:48 +0400

    implemented count method

--- to-entity/src/ru/terraobjects/entity/dao/DAOConsts.java
+++ to-entity/src/ru/terraobjects/entity/dao/DAOConsts.java
@@ -18,17 +18,17 @@ public interface DAOConsts
     public final static String SELECT_PROPERTY_BY_ID = "select * from property where prop_id = ?";
     public final static String SELECT_ALL_OBJECT_TEMPLATE_PROPS = "select * from object_template_props";
     public final static String SELECT_OBJECT_TEMPLATE_PROPS_BY_ID = "select * from object_template_props where "
-	    + "object_template_props_id = ?";
+            + "object_template_props_id = ?";
     public final static String SELECT_OBJECT_TEMPLATE_PROPS_BY_TEMPLATE_ID = "select * from object_template_props where "
-	    + "object_template_id = ?";
+            + "object_template_id = ?";
     public final static String SELECT_ALL_OBJECT_TEMPLATE = "select * from object_template";
     public final static String SELECT_OBJECT_TEMPLATE_BY_ID = "select * from object_template where "
-	    + "object_template_id = ?";
+            + "object_template_id = ?";
     public final static String SELECT_OBJECT_TEMPLATES_BY_PARENT_ID = "select * from object_template where"
-	    + "parent_object_template_id = ?";
+            + "parent_object_template_id = ?";
     public final static String SELECT_OBJECT_PROPS_BY_OBJECT_ID = "select * from object_props where object_id = ?";
     public final static String SELECT_OBJECT_PROP_BY_OBJECT_ID_AND_PROP_ID =
-	    "select * from object_props where object_id = ? AND prop_id = ?";
+            "select * from object_props where object_id = ? AND prop_id = ?";
     public final static String SELECT_ALL_OBJECT_PROPS = "select * from object_props";
     public final static String SELECT_OBJECT_PROPS_BY_PROP_ID = "select * from object_props where prop_id = ?";
     //TODO backup needed before removing
@@ -41,19 +41,20 @@ public interface DAOConsts
     //TODO backup needed here, coz it's delete property from all objects
     public final static String REMOVE_OBJECT_PROP_BY_PROP_ID = "delete from object_props where prop_id = ?";
     public final static String REMOVE_OBJECT_PROP_BY_OBJECT_ID_AND_PROP_ID =
-	    "delete from object_props where object_id = ? AND prop_id = ?";
+            "delete from object_props where object_id = ? AND prop_id = ?";
     public final static String CREATE_PROPS_FOR_OBJECT = "call create_obj_prop(? , ?)";
     public final static String SELECT_OBJPROP_LIST_BY_LISTPROP_ID = "select object_id from object_prop_list "
-	    + "where list_id = ?";
+            + "where list_id = ?";
     public final static String REMOVE_OBJECT_TEMPLATE_BY_ID = "delete from object_template where id = ?";
     public final static String SELECT_OBJECTS_BY_TEMPLATE_ID_PAGING = "select * from object where object_template_id = ? LIMIT ?,?";
     public final static String SELECT_OBJECTS_BY_PARENT_ID_PAGING = "select * from object where parent_object_id = ? LIMIT ?,?";
     public final static String SELECT_OBJECT_BY_TEMPLATE_ID_AND_PROP_ID =
-	    "select obj.* from object obj join object_props op on op.object_id=obj.object_id where obj.object_template_id = ? and op.prop_id = ? ";
+            "select obj.* from object obj join object_props op on op.object_id=obj.object_id where obj.object_template_id = ? and op.prop_id = ? ";
     public final static String SELECT_OBJECT_BY_TEMPLATE_ID_AND_PROP_ID_AND_PROP_VAL =
-	    "select obj.* from object obj join object_props op on op.object_id=obj.object_id where obj.object_template_id = :tid and op.prop_id = :pid "+
-	    " and op.$TYPE$ = :val";
- public final static String SELECT_OBJECTID_BY_PROP_ID_AND_PROP_VAL =
-	    "select object_id from object_props op where op.prop_id = :pid and op.$TYPE$ = :val";
-
+            "select obj.* from object obj join object_props op on op.object_id=obj.object_id where obj.object_template_id = :tid and op.prop_id = :pid "
+            + " and op.$TYPE$ = :val";
+    public final static String SELECT_OBJECTID_BY_PROP_ID_AND_PROP_VAL =
+            "select object_id from object_props op where op.prop_id = :pid and op.$TYPE$ = :val";
+    public final static String COUNT_OBJECTID_BY_PROP_ID_AND_PROP_VAL =
+            "select count(object_id) from object_props op where op.prop_id = :pid and op.$TYPE$ = :val";
 }
--- to-entity/src/ru/terraobjects/entity/dao/TOObjectsHelper.java
+++ to-entity/src/ru/terraobjects/entity/dao/TOObjectsHelper.java
@@ -26,230 +26,223 @@ import ru.terraobjects.entity.manager.TOPropertyManager;
 public class TOObjectsHelper<T>
 {
     private Connection conn;
+    private TOObjectsManager objectsManager;
+    private TOObjectTemplateManager templateManger;
+    private TOObjectPropertyManager propsManager;
+    private TOPropertyManager propMngr;
 
     public TOObjectsHelper(Connection conn)
     {
-	this.conn = conn;
+        this.conn = conn;
+        objectsManager = new TOObjectsManager();
+        templateManger = new TOObjectTemplateManager();
+        propsManager = new TOObjectPropertyManager();
+        propMngr = new TOPropertyManager();
     }
 
     public List<T> loadObjects(Class objectClass)
     {
-	TOObjectsManager objectsManager = new TOObjectsManager();
-	TOObjectTemplateManager templateManger = new TOObjectTemplateManager();
-	if (objectClass.isAnnotationPresent(TemplateId.class))
-	{
-	    Integer tId = new Integer(((TemplateId) (objectClass.getAnnotation(TemplateId.class))).id());
-	    if (tId != null)
-	    {
-		List<TOObject> objects = objectsManager.getAllObjsByTemplate(templateManger.findById(tId));
-		ArrayList<T> ret = new ArrayList<T>();
-		for (TOObject o : objects)
-		{
-		    ret.add(loadObject(objectClass, o));
-		}
-		return ret;
-	    }
-	}
-	return null;
-	//throw new RuntimeException("Can't load objects from DB");
+        if (objectClass.isAnnotationPresent(TemplateId.class))
+        {
+            Integer tId = new Integer(((TemplateId) (objectClass.getAnnotation(TemplateId.class))).id());
+            if (tId != null)
+            {
+                List<TOObject> objects = objectsManager.getAllObjsByTemplate(templateManger.findById(tId));
+                ArrayList<T> ret = new ArrayList<T>();
+                for (TOObject o : objects)
+                {
+                    ret.add(loadObject(objectClass, o));
+                }
+                return ret;
+            }
+        }
+        return null;
+        //throw new RuntimeException("Can't load objects from DB");
     }
 
     public List<T> loadObjects(Class objectClass, Integer page, Integer perPage)
     {
-	TOObjectsManager objectsManager = new TOObjectsManager();
-	TOObjectTemplateManager templateManger = new TOObjectTemplateManager();
-	if (objectClass.isAnnotationPresent(TemplateId.class))
-	{
-	    Integer tId = new Integer(((TemplateId) (objectClass.getAnnotation(TemplateId.class))).id());
-	    if (tId != null)
-	    {
-		List<TOObject> objects = objectsManager.getAllObjsByTemplate(templateManger.findById(tId), page, perPage);
-		ArrayList<T> ret = new ArrayList<T>();
-		for (TOObject o : objects)
-		{
-		    ret.add(loadObject(objectClass, o));
-		}
-		return ret;
-	    }
-	}
-	return null;
-	//throw new RuntimeException("Can't load objects from DB");
+        if (objectClass.isAnnotationPresent(TemplateId.class))
+        {
+            Integer tId = new Integer(((TemplateId) (objectClass.getAnnotation(TemplateId.class))).id());
+            if (tId != null)
+            {
+                List<TOObject> objects = objectsManager.getAllObjsByTemplate(templateManger.findById(tId), page, perPage);
+                ArrayList<T> ret = new ArrayList<T>();
+                for (TOObject o : objects)
+                {
+                    ret.add(loadObject(objectClass, o));
+                }
+                return ret;
+            }
+        }
+        return null;
+        //throw new RuntimeException("Can't load objects from DB");
     }
 
     public T loadObject(Class objectClass, Integer oid)
     {
-	TOObject obj = new TOObjectsManager().findById(oid);
-	if (obj != null)
-	{
-	    return loadObject(objectClass, obj);
-	} else
-	{
-	    return null;
-	}
+        TOObject obj = objectsManager.findById(oid);
+        if (obj != null)
+        {
+            return loadObject(objectClass, obj);
+        } else
+        {
+            return null;
+        }
     }
 
     public T loadObject(Class objectClass, TOObject obj)
     {
-	T retObj = null;
-	try
-	{
-	    //loading object from db with props and construct new Object
-	    retObj = (T) objectClass.newInstance();
-	    TOObjectPropertyManager propsManager = new TOObjectPropertyManager();
-	    TOPropertyManager propMngr = new TOPropertyManager();
-	    if (obj != null)
-	    {
-		for (Method m : retObj.getClass().getMethods())
-		{
-		    if (m.isAnnotationPresent(PropSetter.class))
-		    {
-			//get method setter and set to retObj info from obj
-			Integer propId = Integer.valueOf(m.getAnnotation(PropSetter.class).id());
-			m.invoke(retObj, propsManager.getPropertyValue(obj, propMngr.findById(propId)));
-		    }
-		}
-	    } else
-	    {
-		Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE,
-			"Can't load obj from DB, id: " + obj.toString(), new Object[]
-			{
-			});
-	    }
+        T retObj = null;
+        try
+        {
+            //loading object from db with props and construct new Object
+            retObj = (T) objectClass.newInstance();
+            if (obj != null)
+            {
+                for (Method m : retObj.getClass().getMethods())
+                {
+                    if (m.isAnnotationPresent(PropSetter.class))
+                    {
+                        //get method setter and set to retObj info from obj
+                        Integer propId = Integer.valueOf(m.getAnnotation(PropSetter.class).id());
+                        m.invoke(retObj, propsManager.getPropertyValue(obj, propMngr.findById(propId)));
+                    }
+                }
+            } else
+            {
+                Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE,
+                        "Can't load obj from DB, id: " + obj.toString(), new Object[]
+                        {
+                        });
+            }
 
-	} catch (IllegalArgumentException ex)
-	{
-	    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-	} catch (InvocationTargetException ex)
-	{
-	    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-	} catch (InstantiationException ex)
-	{
-	    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-	} catch (IllegalAccessException ex)
-	{
-	    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-	}
-	return retObj;
+        } catch (IllegalArgumentException ex)
+        {
+            Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex)
+        {
+            Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex)
+        {
+            Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex)
+        {
+            Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retObj;
     }
 
     public TOObject storeObject(T objToStore, Boolean storedProc)
     {
-	//deconstruct object to props and store it into db
-	TOObject ret = null;
-	TOObjectsManager objManager = new TOObjectsManager();
-	TOObjectPropertyManager propsManager = new TOObjectPropertyManager();
-	TOPropertyManager propMngr = new TOPropertyManager();
-	//get templateId  from object
-	TemplateId templateIdAnnotation = objToStore.getClass().getAnnotation(TemplateId.class);
-	if (templateIdAnnotation != null)
-	{
-	    Integer tId = Integer.valueOf(templateIdAnnotation.id());
-	    ret = objManager.createNewObject(tId);
-	    //prosManager.createDefaultPropsForObject(tId, retId, storedProc);
-	    List<TOObjectProperty> props = new ArrayList<TOObjectProperty>();
-	    TOObjectTemplateManager templateManger = new TOObjectTemplateManager();
-	    for (Method m : objToStore.getClass().getMethods())
-	    {
-		if (m.isAnnotationPresent(PropGetter.class))
-		{
-		    //get method getter and set to retObj info from obj
-		    Integer propId = m.getAnnotation(PropGetter.class).id();
-		    TOProperty prop = propMngr.findById(propId);
-		    Integer propType = m.getAnnotation(PropGetter.class).type();
-		    Object val = null;
-		    if (Boolean.valueOf(m.getAnnotation(PropGetter.class).autoincrement()))
-		    {
-			Integer start = Integer.valueOf(m.getAnnotation(PropGetter.class).startNum());
-			Long count = objManager.getObjectsCountByTemplateId(templateManger.findById(tId));
-			if (count == 0)
-			{
-			    count = new Long(start);
-			} else
-			{
-			    count += 1;
-			}
-			val = count;
-		    } else
-		    {
-			try
-			{
-			    val = m.invoke(objToStore, new Object[]
-				    {
-				    });
-			} catch (IllegalAccessException ex)
-			{
-			    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-			} catch (IllegalArgumentException ex)
-			{
-			    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-			} catch (InvocationTargetException ex)
-			{
-			    Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		    }
-		    TOObjectPropsId id = new TOObjectPropsId(0, ret.getObjectId(), propId);
-		    if (!storedProc)
-		    {
-			propsManager.createNewObjectPropertyWithValue(ret, prop, val, propType);
-		    } else
-		    {
-			TOObjectProperty newProp = new TOObjectProperty(id, ret, prop, propType);
-			TOObjectPropertyManager.setPropValue(newProp, val, propType);
-			props.add(newProp);
-		    }
-		}
-	    }
-	    if (!props.isEmpty())
-	    {
-		propsManager.bulkCreateObjectProps(props);
-	    }
-	}
-	return ret;
+        //deconstruct object to props and store it into db
+        TOObject ret = null;
+        //get templateId  from object
+        TemplateId templateIdAnnotation = objToStore.getClass().getAnnotation(TemplateId.class);
+        if (templateIdAnnotation != null)
+        {
+            Integer tId = Integer.valueOf(templateIdAnnotation.id());
+            ret = objectsManager.createNewObject(tId);
+            //prosManager.createDefaultPropsForObject(tId, retId, storedProc);
+            List<TOObjectProperty> props = new ArrayList<>();
+            for (Method m : objToStore.getClass().getMethods())
+            {
+                if (m.isAnnotationPresent(PropGetter.class))
+                {
+                    //get method getter and set to retObj info from obj
+                    Integer propId = m.getAnnotation(PropGetter.class).id();
+                    TOProperty prop = propMngr.findById(propId);
+                    Integer propType = m.getAnnotation(PropGetter.class).type();
+                    Object val = null;
+                    if (Boolean.valueOf(m.getAnnotation(PropGetter.class).autoincrement()))
+                    {
+                        Integer start = Integer.valueOf(m.getAnnotation(PropGetter.class).startNum());
+                        Long count = objectsManager.getObjectsCountByTemplateId(templateManger.findById(tId));
+                        if (count == 0)
+                        {
+                            count = new Long(start);
+                        } else
+                        {
+                            count += 1;
+                        }
+                        val = count;
+                    } else
+                    {
+                        try
+                        {
+                            val = m.invoke(objToStore, new Object[]
+                                    {
+                                    });
+                        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex)
+                        {
+                            Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    TOObjectPropsId id = new TOObjectPropsId(0, ret.getObjectId(), propId);
+                    if (!storedProc)
+                    {
+                        propsManager.createNewObjectPropertyWithValue(ret, prop, val, propType);
+                    } else
+                    {
+                        TOObjectProperty newProp = new TOObjectProperty(id, ret, prop, propType);
+                        TOObjectPropertyManager.setPropValue(newProp, val, propType);
+                        props.add(newProp);
+                    }
+                }
+            }
+            if (!props.isEmpty())
+            {
+                propsManager.bulkCreateObjectProps(props);
+            }
+        }
+        return ret;
     }
 
     public void updateObject(T objToStore, Integer oId)
     {
-	TOObjectsManager objManager = new TOObjectsManager();
-	TOObjectPropertyManager propsManager = new TOObjectPropertyManager();
-	TOPropertyManager propMngr = new TOPropertyManager();
-	TemplateId templateIdAnnotation = objToStore.getClass().getAnnotation(TemplateId.class);
-	if (templateIdAnnotation != null)
-	{
-	    //  Integer tId = Integer.valueOf(templateIdAnnotation.id());
-	    TOObject obj = objManager.findById(oId);
-	    for (Method m : objToStore.getClass().getMethods())
-	    {
-		if (m.isAnnotationPresent(PropGetter.class))
-		{
-		    Integer propId = Integer.valueOf(m.getAnnotation(PropGetter.class).id());
-		    Integer propType = m.getAnnotation(PropGetter.class).type();
-		    Object val = null;
-		    try
-		    {
-			val = m.invoke(objToStore, new Object[]
-				{
-				});
+        TOObjectsManager objManager = new TOObjectsManager();
+        TemplateId templateIdAnnotation = objToStore.getClass().getAnnotation(TemplateId.class);
+        if (templateIdAnnotation != null)
+        {
+            //  Integer tId = Integer.valueOf(templateIdAnnotation.id());
+            TOObject obj = objManager.findById(oId);
+            for (Method m : objToStore.getClass().getMethods())
+            {
+                if (m.isAnnotationPresent(PropGetter.class))
+                {
+                    Integer propId = Integer.valueOf(m.getAnnotation(PropGetter.class).id());
+                    Integer propType = m.getAnnotation(PropGetter.class).type();
+                    Object val = null;
+                    try
+                    {
+                        val = m.invoke(objToStore, new Object[]
+                                {
+                                });
 
-		    } catch (IllegalAccessException ex)
-		    {
-			Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-		    } catch (IllegalArgumentException ex)
-		    {
-			Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-		    } catch (InvocationTargetException ex)
-		    {
-			Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
-		    }
-		    TOProperty prop = propMngr.findById(propId);
-		    propsManager.setPropertyValue(obj, prop, val, propType);
-		}
-	    }
-	}
+                    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex)
+                    {
+                        Logger.getLogger(TOObjectsHelper.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    TOProperty prop = propMngr.findById(propId);
+                    propsManager.setPropertyValue(obj, prop, val, propType);
+                }
+            }
+        }
     }
 
     public List<Integer> findObjectsByField(Integer propId, Object val, Integer type)
     {
-	TOObjectsManager objManager = new TOObjectsManager();
-	return objManager.getObjectsByPropAndPropVal(propId, type, val);
+        return objectsManager.getObjectsByPropAndPropVal(propId, type, val);
+    }
+
+    public Long countObjectsByField(Integer propId, Object val, Integer type)
+    {
+        return objectsManager.getObjectsCountByPropAndPropVal(propId, type, val);
+    }
+
+    public Long countObjectsByTemplate(Integer templateId)
+    {
+        return objectsManager.getObjectsCountByTemplateId(templateManger.findById(templateId));
     }
 }
